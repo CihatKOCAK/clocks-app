a250990618ea6c80442d0d9a11d257978aabc1f3
Cihat KOÇAK <pcihatkocakks@gmail.com>
2022-09-15 10:41:14 +0300

    calendar,DigitalClockCreated

---
+++ src/components/Calendar/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./style.scss";
+
+function Calendar() {
+
+
+  return (
+    <div className="analog-clock-body">
+      Calendar Here
+    </div>
+  );
+}
+
+export default Calendar;
---
+++ src/components/DigitalClock/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./style.scss";
+
+function DigitalClock() {
+
+
+  return (
+    <div className="analog-clock-body">
+      DigitalClock Here
+    </div>
+  );
+}
+
+export default DigitalClock;
